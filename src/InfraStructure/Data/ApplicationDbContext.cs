98c57a1ac7e3ba510d0700e7da6e8877a253ab21
mahesh <jadhao.mahesh1009@gmail.com>
2026-07-18 12:27:59 +0530

    DbContect  added

---
+++ src/InfraStructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+
+   
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ):base (options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
+ 
