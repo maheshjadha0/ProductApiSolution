fd5e903932b4d18b8758e1989400d0b4c2bf7056
mahesh <jadhao.mahesh1009@gmail.com>
2026-07-18 16:53:46 +0530

    dtos

---
+++ src/Application/DTOs/Product/ItemDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Product
+{
+    public class ItemDto
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ src/Application/DTOs/Product/ProductResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Product
+{
+    public class ProductResponseDto
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public List<ItemDto> Items { get; set; }
+    }
+}
